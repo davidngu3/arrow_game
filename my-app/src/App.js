53d0149b9639753ca6a0fb43beb5a1a73617489d
David <d.nguyen.melb@gmail.com>
2022-04-27 13:52:36 +1000

    start button disabled when start game

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,21 @@
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [gameActive, setGameState] = useState(false)
+
+  function startGame(e) {
+    setGameState(true)
+    console.log("Game started")
+  }
+
   return (
     <div className="app-container">
       <div className="game-container">
         <div className="title-bar">
           ARROW GAME
         </div>
-        <button className="start-button">
+        <button className="start-button" onClick={startGame} disabled={gameActive}>
           START BUTTON
         </button>
         <ArrowContainer />

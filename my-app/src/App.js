20ecca1f0adb8c6934ea8f2bb484722ea81cb9d4
David <d.nguyen.melb@gmail.com>
2022-04-27 13:40:52 +1000

    colour and layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,22 +2,26 @@ import './App.css';
 
 function App() {
   return (
-        <div className="app-container">
-          <div className="title-bar">
-            ARROW GAME
-          </div>
-          <ArrowContainer />
-          <div className="space-button">
-            SPACE
-          </div>
+    <div className="app-container">
+      <div className="game-container">
+        <div className="title-bar">
+          ARROW GAME
         </div>
+        <button className="start-button">
+          START BUTTON
+        </button>
+        <ArrowContainer />
+        <div className="space-button">
+          SPACE
+        </div>
+      </div>
+    </div>
   );
 }
 
 function ArrowContainer() {
   return (
     <div className="arrow-container">
-      ARROW CONTAINER
     </div>
   )
 }

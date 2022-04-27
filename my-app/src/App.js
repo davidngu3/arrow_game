1512489b8df2ba07e7d86596629298cdb8cc11c5
David <d.nguyen.melb@gmail.com>
2022-04-27 13:22:46 +1000

    create base html

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,24 @@ import './App.css';
 
 function App() {
   return (
-        <p>
-          hello
-        </p>
+        <div className="app-container">
+          <div className="title-bar">
+            ARROW GAME
+          </div>
+          <ArrowContainer />
+          <div className="space-button">
+            SPACE
+          </div>
+        </div>
   );
+}
+
+function ArrowContainer() {
+  return (
+    <div className="arrow-container">
+      ARROW CONTAINER
+    </div>
+  )
 }
 
 export default App;
